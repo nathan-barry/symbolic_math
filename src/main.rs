a2c8f0e12529e01634b245f690d33898a7e6051a
Nathan Barry <nathanbarry474@gmail.com>
2023-07-30 09:27:00 -0500

    test operations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use symbolic_math::expr::Expr;
+
 fn main() {
-    println!("Hello world");
+    let x = Expr::new_var("x");
+    let y = Expr::new_var("y");
+    let c = Expr::new_val(72.0);
+    let p = Expr::new_val(5.0);
+    println!("{}", x);
+    println!("{}", y);
+    println!("{}", c);
+    println!("{}", x+y*c.pow(p));
 }
