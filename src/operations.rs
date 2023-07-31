31b4a20166e3b25101d2c763f49dfcf909d2a661
Nathan Barry <nathanbarry474@gmail.com>
2023-07-30 19:42:46 -0500

    added ops with f64

--- src/operations.rs
+++ src/operations.rs
@@ -8,7 +8,7 @@ impl Expr {
     }
 }
 
-// Overload Operation implementations
+// Add Overload Operation implementations
 impl ops::Add for Expr {
     type Output = Expr;
 
@@ -17,6 +17,23 @@ impl ops::Add for Expr {
     }
 }
 
+impl ops::Add<f64> for Expr {
+    type Output = Expr;
+
+    fn add(self, rhs: f64) -> Expr {
+        Expr::Add(Box::new(self), Box::new(Expr::new_val(rhs)))
+    }
+}
+
+impl ops::Add<Expr> for f64 {
+    type Output = Expr;
+
+    fn add(self, rhs: Expr) -> Expr {
+        Expr::Add(Box::new(Expr::new_val(self)), Box::new(rhs))
+    }
+}
+
+// Sub Overload Operation implementations
 impl ops::Sub for Expr {
     type Output = Expr;
 
@@ -25,6 +42,23 @@ impl ops::Sub for Expr {
     }
 }
 
+impl ops::Sub<f64> for Expr {
+    type Output = Expr;
+
+    fn sub(self, rhs: f64) -> Expr {
+        Expr::Sub(Box::new(self), Box::new(Expr::new_val(rhs)))
+    }
+}
+
+impl ops::Sub<Expr> for f64 {
+    type Output = Expr;
+
+    fn sub(self, rhs: Expr) -> Expr {
+        Expr::Sub(Box::new(Expr::new_val(self)), Box::new(rhs))
+    }
+}
+
+// Mul Overload Operation implementations
 impl ops::Mul for Expr {
     type Output = Expr;
 
@@ -33,6 +67,23 @@ impl ops::Mul for Expr {
     }
 }
 
+impl ops::Mul<f64> for Expr {
+    type Output = Expr;
+
+    fn mul(self, rhs: f64) -> Expr {
+        Expr::Mul(Box::new(self), Box::new(Expr::new_val(rhs)))
+    }
+}
+
+impl ops::Mul<Expr> for f64 {
+    type Output = Expr;
+
+    fn mul(self, rhs: Expr) -> Expr {
+        Expr::Mul(Box::new(Expr::new_val(self)), Box::new(rhs))
+    }
+}
+
+// Div Overload Operation implementations
 impl ops::Div for Expr {
     type Output = Expr;
 
@@ -41,10 +92,60 @@ impl ops::Div for Expr {
     }
 }
 
+impl ops::Div<f64> for Expr {
+    type Output = Expr;
+
+    fn div(self, rhs: f64) -> Expr {
+        Expr::Div(Box::new(self), Box::new(Expr::new_val(rhs)))
+    }
+}
+
+impl ops::Div<Expr> for f64 {
+    type Output = Expr;
+
+    fn div(self, rhs: Expr) -> Expr {
+        Expr::Div(Box::new(Expr::new_val(self)), Box::new(rhs))
+    }
+}
+
+// Neg Overload Operation implementations
 impl ops::Neg for Expr {
     type Output = Expr;
 
     fn neg(self) -> Expr {
         Expr::Neg(Box::new(self))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let x = Expr::new_var("x");
+        assert_eq!(x.clone() + Expr::new_val(2.0), x.clone() + 2.0);
+        assert_eq!(Expr::new_val(2.0) + x.clone(), 2.0 + x.clone());
+    }
+
+    #[test]
+    fn test_sub() {
+        let x = Expr::new_var("x");
+        assert_eq!(x.clone() - Expr::new_val(2.0), x.clone() - 2.0);
+        assert_eq!(Expr::new_val(2.0) - x.clone(), 2.0 - x.clone());
+    }
+
+    #[test]
+    fn test_mul() {
+        let x = Expr::new_var("x");
+        assert_eq!(x.clone() * Expr::new_val(2.0), x.clone() * 2.0);
+        assert_eq!(Expr::new_val(2.0) * x.clone(), 2.0 * x.clone());
+    }
+
+    #[test]
+    fn test_div() {
+        let x = Expr::new_var("x");
+        assert_eq!(x.clone() / Expr::new_val(2.0), x.clone() / 2.0);
+        assert_eq!(Expr::new_val(2.0) / x.clone(), 2.0 / x.clone());
     }
 }
